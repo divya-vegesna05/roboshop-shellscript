ccd056834d6cd07164c73ebc622498e60d91e22d
divya <divya.vegesna55@gmail.com>
2024-04-23 21:41:50 +0530

    install mysql

--- web.sh
+++ web.sh
@@ -26,7 +26,7 @@ VALIDATE $? "Installing nginx"
 systemctl enable nginx &>> $LOGFILE
 VALIDATE $? "enabling nginx"
 systemctl start nginx &>> $LOGFILE
-VALIDATE $? starting nginx"
+VALIDATE $? "starting nginx"
 rm -rf /usr/share/nginx/html/* &>> $LOGFILE
 VALIDATE $? "removing default html files"
 curl -o /tmp/web.zip https://roboshop-builds.s3.amazonaws.com/web.zip &>> $LOGFILE
